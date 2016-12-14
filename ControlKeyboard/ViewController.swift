18d4e0755e873f591365ac0a3f0d46c23010dc72
Xuehua Chen <xuehuachen@Xuehuas-MacBook-Pro.local>
2016-12-13 21:58:01 -0800

    first commit

---
+++ ControlKeyboard/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ControlKeyboard
+//
+//  Created by Xuehua Chen on 12/13/16.
+//  Copyright © 2016 Xuehua Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
